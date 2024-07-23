9b31faf95e8bb49bcb8e541febc39b506229a75c
skinogodest <165472263+skinogodest@users.noreply.github.com>
2024-07-23 14:56:13 +0100

    Add files via upload

---
+++ functionality.js
@@ -0,0 +1,18 @@
+let number = 0
+const TextDocument = document.getElementById("ColorText")
+const page =document.getElementById("Tag")
+const btn =  document.getElementById("LogButton")
+let 
+function ChangeColor(a,b){
+
+TextDocument.style.color = "red"
+TextDocument.innerHTML = "I love coding website for my future"
+
+page.style.color = "green"
+
+}
+
+
+
+
+
